aa309dedebd504a5b1dc787d9489cc628d70b978
ashwin <ash.the.reaper@gmail.com>
2017-07-23 22:54:25 +0400

    passenger manage request 90% ok

--- Rod1Lift/app/src/test/java/rode1lift/ashwin/uomtrust/mu/rod1lift/ExampleUnitTest.java
+++ Rod1Lift/app/src/test/java/rode1lift/ashwin/uomtrust/mu/rod1lift/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package rode1lift.ashwin.uomtrust.mu.rod1lift;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
